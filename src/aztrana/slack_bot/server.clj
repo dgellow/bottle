328bc8c73bb840e7205634bb7b181232a7f7a740
dgellow <samuel.elborai@gmail.com>
2015-12-10 08:44:50 +0700

    man... forget to pass user as a param

--- src/aztrana/slack_bot/server.clj
+++ src/aztrana/slack_bot/server.clj
@@ -113,7 +113,7 @@
 (defmethod interpret :default
   [{:keys [user]}]
   {:message
-   (if (= "sako" (state/id->name))
+   (if (= "sako" (state/id->name user))
      "uh?"
      "Sorry mate, my actions are limited. Ask me to `help` you if you want to see what I can do for you.")})
 
